735dd583111249f4be842a402a5c03f75f9a9666
AanandBhandari <aanandbhandari143@gmail.com>
2018-08-29 23:42:32 +0545

    added faker module

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,4 +1,6 @@
 let router = require('express').Router();
+const faker = require('faker');
+const Post = require('../../models/Posts');
 // over-ridding the default layout
 router.all('/*',(req,res,next) => {
     req.app.locals.layout ='admin';
@@ -14,5 +16,18 @@ router.get('/dashboard',(req,res) => {
     res.render('admin/dashboard');
     // res.send('hellowworld');
 });
+router.post('/generate-fake-posts',(req,res) => {
+    for (let i = 0; i <req.body.amount; i++) {
+        let post = new Post();
+        post.title = faker.name.title();
+        post.status = 'public';
+        post.allowComments = faker.random.boolean();
+        post.body = faker.lorem.sentence();
+        post.save();
+
+        
+    }
+    res.redirect('/admin/posts')
+})
 
 module.exports = router;
