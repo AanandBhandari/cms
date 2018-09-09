77c0d87567bf08de9e3961f7a9e80643f9904a0b
AanandBhandari <aanandbhandari143@gmail.com>
2018-09-09 23:50:16 +0545

    make it better

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoDbUrl : 'mongodb://localhost:27017/cms'
+    // mongoDbUrl : 'mongodb://localhost:27017/cms'
+    mongoDbUrl : 'mongodb://cms-user:helloworld1@ds111562.mlab.com:11562/cms'
 }
