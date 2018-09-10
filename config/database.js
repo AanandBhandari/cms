a4e76bdfc157d729ebc771bbdd4020da9575a515
AanandBhandari <aanandbhandari143@gmail.com>
2018-09-10 10:29:58 +0545

    setup env production variable to heroku'

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
-module.exports = {
-    // mongoDbUrl : 'mongodb://localhost:27017/cms'
-    mongoDbUrl : 'mongodb://cms-user:helloworld1@ds111562.mlab.com:11562/cms'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prod-database');
+} else {
+    module.exports = require('./dev-database');
 }
