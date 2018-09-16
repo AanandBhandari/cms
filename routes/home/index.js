6e03139d0c0c2bb8809d7390f827282bd909156b
AanandBhandari <aanandbhandari143@gmail.com>
2018-09-16 20:27:32 +0545

    cb uri

--- routes/home/index.js
+++ routes/home/index.js
@@ -160,7 +160,7 @@ passport.use(
     new googleStrategy({
         clientID: keys.clientID ,
         clientSecret: keys.clientSecret,
-        callbackURL: "http://postany.herokuapp.com/admin"
+        callbackURL: "http://postany.herokuapp.com/google/redirect"
 },(accessToken,refreshToken,profile,done) => {
     // passport callback function
     // cheak user if already exist in db
